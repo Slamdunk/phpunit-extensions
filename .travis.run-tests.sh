eaaf6892512dedab0a876d219aa1f2bcc828e195
Filippo Tessarotto <zoeslam@gmail.com>
2017-05-06 17:29:58 +0200

    Do not fail on coverage upload fail

--- .travis.run-tests.sh
+++ .travis.run-tests.sh
@@ -3,10 +3,10 @@ set -ev
 if [ "$PHP_LATEST_VERSION" = 1 ]
 then
     vendor/bin/phpunit --coverage-clover ./clover.xml
-    wget https://scrutinizer-ci.com/ocular.phar
-    php ocular.phar code-coverage:upload --format=php-clover ./clover.xml
     phpenv config-rm xdebug.ini || return 0
     vendor/bin/php-cs-fixer --diff --dry-run --verbose fix
+    wget https://scrutinizer-ci.com/ocular.phar || return 0
+    php ocular.phar code-coverage:upload --format=php-clover ./clover.xml || return 0
 else
     vendor/bin/phpunit
 fi
