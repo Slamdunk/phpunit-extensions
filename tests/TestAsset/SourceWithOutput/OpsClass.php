96379cb04d1cd877a138514f937291dd4290d9e7
Filippo Tessarotto <zoeslam@gmail.com>
2017-05-10 12:23:34 +0200

    Missing files

---
+++ tests/TestAsset/SourceWithOutput/OpsClass.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Slam\PHPUnit\Tests\TestAsset\SourceWithOutput;
+
+final class OpsClass
+{
+}
+
+echo PHP_EOL;
---
+++ tests/TestAsset/ValidPsr0/ValidInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+interface Slam_PHPUnit_Tests_TestAsset_ValidPsr0_ValidInterface
+{
+}
